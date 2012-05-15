00125e65808531e09b4d0a16a84828ed28a17a47
Morteza Ansarinia <ansarinia@me.com>
2012-05-16 01:01:00 +0430

    Cleaned up document repository

--- src/fuschia/tagger/common/DocumentRepository.java
+++ src/fuschia/tagger/common/DocumentRepository.java
@@ -7,56 +7,57 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ResourceBundle;
 import java.util.zip.Deflater;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
-import javax.imageio.stream.FileImageInputStream;
-
-
 public class DocumentRepository {
-	private HashMap<String, Document> docs;
-	
+	private HashMap<String, Document> docs = null;
+
 	public DocumentRepository() {
 		docs = new HashMap<String, Document>();
 	}
-	
+
 	public void setDocuments(HashMap<String, Document> docs) {
 		this.docs.clear();
 		this.docs.putAll(docs);
 	}
-	
-	
-	
+
+	@SuppressWarnings("unchecked")
 	public static DocumentRepository get911() {
 		try{
 			DocumentRepository repo = new DocumentRepository();
 			InputStream is = repo.getClass().getResourceAsStream("911.cmap.gz");
 			GZIPInputStream gzipis = new GZIPInputStream(is);
 			ObjectInputStream oisCompressed = new ObjectInputStream(gzipis);
-			
-			repo.setDocuments((HashMap<String, Document>)oisCompressed.readObject());
-			
+
+			Object obj = oisCompressed.readObject();
+
+			if (obj instanceof HashMap<?, ?>) {
+				repo.setDocuments((HashMap<String, Document>)obj);
+			} else {
+				throw new Exception("Invalid object format");
+			}
+
 			return repo;					
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	public Map<String, Document> getAll() {
 		return docs;
 	}
-	
-	public Document getDocument(String documentId) {
+
+	public Document getDocumentById(String documentId) {
 		return docs.get(documentId);
 	}
-	
+
 	public void addDocument(String documentId, Document document) {
 		docs.put(documentId, document);
 	}
-	
+
 	public void saveToFile(String strFilePath) throws Exception{
 		FileOutputStream fout = new FileOutputStream(new File(strFilePath), false);
 		GZIPOutputStream gzipos = new GZIPOutputStream(fout) {
@@ -72,17 +73,25 @@ public class DocumentRepository {
 		fout.flush();
 		fout.close();		
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public void loadFromFile(String strFilePath) throws Exception {
-		 FileInputStream fis = new FileInputStream(new File(strFilePath));
-		 GZIPInputStream gzipis = new GZIPInputStream(fis);
-		 ObjectInputStream oisCompressed = new ObjectInputStream(gzipis);
-		 
-		 docs.clear();
-		 docs = null;
-		 docs = (HashMap<String, Document>)oisCompressed.readObject();
+		FileInputStream fis = new FileInputStream(new File(strFilePath));
+		GZIPInputStream gzipis = new GZIPInputStream(fis);
+		ObjectInputStream oisCompressed = new ObjectInputStream(gzipis);
+
+		docs.clear();
+		docs = null;
+
+		Object obj = oisCompressed.readObject();
+
+		if (obj instanceof HashMap<?, ?>) {
+			docs = (HashMap<String, Document>)obj;
+		} else {
+			throw new Exception("Invalid object format");
+		}
 	}
-	
+
 	public void clear() {
 		docs.clear();
 	}
