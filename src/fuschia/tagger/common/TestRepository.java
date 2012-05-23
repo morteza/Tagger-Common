ab71f965df758c007318421cf62aeab725ca4b07
Morteza Ansarinia <ansarinia@me.com>
2012-05-23 10:19:11 +0430

    test 911 repo commit
    added a line to query and test imported compiled 911 repository
    
    Signed-off-by: Morteza Ansarinia <ansarinia@me.com>

--- src/fuschia/tagger/common/TestRepository.java
+++ src/fuschia/tagger/common/TestRepository.java
@@ -5,6 +5,7 @@ public class TestRepository {
 	public static void main(String[] args) {
 		DocumentRepository repo = DocumentRepository.get911();
 		System.out.println(repo.getAll().size());
+		System.out.println("GU002-Q1 "+repo.getDocumentById("GU001-Q1").getText());
 	}
 
 }
